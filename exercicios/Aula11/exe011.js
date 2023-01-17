d71486024ad88ce97c7a4a8ebb581e6329981e27
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-01-17 15:23:03 -0300

    Atualização das aulas

---
+++ exercicios/Aula11/exe011.js
@@ -0,0 +1,9 @@
+var idade = 12
+console.log (`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log ('Não Vota')
+} else  if (idade<18) { 
+    console.log ('Voto opicional')
+} else {
+    console.log ('Voto Obrigatorio')
+}
