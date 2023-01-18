bb7abcc4e932c46f09db37e38ad80dd3b8b016af
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-01-18 10:01:08 -0300

    Atualização

---
+++ exercicios/Aula14/anbiente.js
@@ -0,0 +1,12 @@
+/*Estrutura de repitição  for */
+
+/*for (inicio;condição;incremento)*/
+
+console.log('Vai começar?')
+for(var c = 1; c <= 3; c++){
+    console.log(`Tudo bem ! ${c}`)
+}
+console.log ('Sim')
+
+
+
